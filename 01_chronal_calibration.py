856544cb7e3b47a5caa53f7fb884878c8aa5eb03
Daniel Cardoza <dan@danielcardoza.com>
2018-12-01 18:29:32 -0800

    Clean up day 1 solution.
    Use itertools.accumulate() for building a
    prefix sum array. Add comments and better names
    for variables.

--- 01_chronal_calibration.py
+++ 01_chronal_calibration.py
@@ -1,27 +1,24 @@
+from itertools import accumulate
 import sys
 
-def changes():
-    return [int(line) for line in open('01_input.txt')]
+def changes(input_file='01_input.txt'):
+    """Return a list of frequency changes"""
+    return [int(line) for line in open(input_file)]
 
-def prefix_array(a):
-    p = [a[0]]
-    for i in range(1, len(a)):
-        p.append(p[-1] + a[i])
-    return p
 
 def pair(a, divisor):
-    """Find a pair of numbers divisble by divisor.
-    Return the indices of the pair with the smallest quotient"""
-    rs = []
+    """Find a pair of numbers in a divisble by divisor.
+    Return the indices of the pair in a with the smallest quotient"""
+    quotient = lambda x, y: abs(x -y) / divisor
+    indices = None
     for i in range(0, len(a) - 1):
         for j in range(i + 1, len(a)):
             if abs(a[j] - a[i]) % divisor == 0:
-                quotient = abs(a[j] - a[i]) / divisor
-                if not rs:
-                    rs = (i, j)
-                elif quotient < (abs(a[rs[0]] - a[rs[1]]) / divisor):
-                    rs = (i, j)
-    return rs
+                if not indices:
+                    indices = (i, j)
+                elif quotient(a[i], a[j]) < quotient(a[indices[0]], a[indices[1]]):
+                    indices = (i, j)
+    return indices
 
 # Part 1
 c = changes()
@@ -29,24 +26,9 @@ c_sum = sum(c)
 print(f'Sum is {c_sum}')
 
 # Part 2
-p = prefix_array(c)
+p = list(accumulate(c))
 i, j = pair(p, c_sum)
-diff = abs(p[j] - p[i])
+diff = abs(p[i] - p[j])
 quotient = diff / c_sum
 print(f'Pair is ({i},{j}) with quotient {quotient}')
 print(f'Answer is {p[max(i, j)]}')
-sys.exit(1)
-
-# Old Part 2
-i = 0
-total = 0
-length = len(changes)
-frequency = set([total])
-while True:
-    print(i, changes[i])
-    total += changes[i]
-    if total in frequency:
-        print(total)
-        print('here')
-        break
-    i = (i + 1) % length
